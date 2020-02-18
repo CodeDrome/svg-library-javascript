da8b1abdc58e14d4084ac88b140a6ff3419d73f8
Chris Webb <50113303+CodeDrome@users.noreply.github.com>
2020-02-18 12:52:51 +0000

    Add files via upload

--- svglibrary.js
+++ svglibrary.js
@@ -1,33 +1,16 @@
 ﻿class SVGLibrary
 {
-	static clear(id)
-	{
-		document.getElementById(id).innerHTML = "";
-	}
-
-
-	static circle(id, strokeColor, strokeWidth, fillColor, r, cx, cy)
-	{
-    	const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-
-    	circle.setAttributeNS(null, "cx", cx);
-    	circle.setAttributeNS(null, "cy", cy);
-    	circle.setAttributeNS(null, "r", r);
-    	circle.setAttributeNS(null, "stroke", strokeColor);
-    	circle.setAttributeNS(null, "fill", fillColor);
-    	circle.setAttributeNS(null, "stroke-width", strokeWidth);
-
-    	document.getElementById(id).appendChild(circle);
-  	}
-
-
 	static fill(id, color)
 	{
     	document.getElementById(id).style.backgroundColor = color;
 	}
 
+	static clear(id)
+	{
+		document.getElementById(id).innerHTML = "";
+	}
 
-	static line(id, strokeColor, strokeWidth, x1, y1, x2, y2)
+	static line(id, x1, y1, x2, y2, classname, strokeColor, strokeWidth)
 	{
     	const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
 
@@ -35,14 +18,43 @@
     	line.setAttributeNS(null, "y1", y1);
     	line.setAttributeNS(null, "x2", x2);
     	line.setAttributeNS(null, "y2", y2);
-    	line.setAttributeNS(null, "stroke", strokeColor);
-    	line.setAttributeNS(null, "stroke-width", strokeWidth);
+
+		if(classname !== null)
+		{
+    		line.setAttributeNS(null, "class", classname);
+		}
+		else
+		{
+    		line.setAttributeNS(null, "stroke", strokeColor);
+    		line.setAttributeNS(null, "stroke-width", strokeWidth);
+		}
 
     	document.getElementById(id).appendChild(line);
 	}
 
+	static circle(id, cx, cy, r, classname, fillColor, strokeColor, strokeWidth)
+	{
+    	const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 
-	static ellipse(id, cx, cy, rx, ry, fillColor, strokeColor, strokeWidth)
+    	circle.setAttributeNS(null, "cx", cx);
+    	circle.setAttributeNS(null, "cy", cy);
+    	circle.setAttributeNS(null, "r", r);
+
+		if(classname !== null)
+		{
+    		circle.setAttributeNS(null, "class", classname);
+		}
+		else
+		{
+    		circle.setAttributeNS(null, "stroke", strokeColor);
+    		circle.setAttributeNS(null, "fill", fillColor);
+    		circle.setAttributeNS(null, "stroke-width", strokeWidth);
+		}
+
+    	document.getElementById(id).appendChild(circle);
+  	}
+
+	static ellipse(id, cx, cy, rx, ry, classname, fillColor, strokeColor, strokeWidth)
 	{
     	const ellipse = document.createElementNS("http://www.w3.org/2000/svg", "ellipse");
 
@@ -50,15 +62,22 @@
     	ellipse.setAttributeNS(null, "cy", cy);
     	ellipse.setAttributeNS(null, "rx", rx);
     	ellipse.setAttributeNS(null, "ry", ry);
-    	ellipse.setAttributeNS(null, "stroke", strokeColor);
-    	ellipse.setAttributeNS(null, "fill", fillColor);
-    	ellipse.setAttributeNS(null, "stroke-width", strokeWidth);
+
+		if(classname !== null)
+		{
+			ellipse.setAttributeNS(null, "class", classname);
+		}
+		else
+		{
+    		ellipse.setAttributeNS(null, "stroke", strokeColor);
+    		ellipse.setAttributeNS(null, "fill", fillColor);
+    		ellipse.setAttributeNS(null, "stroke-width", strokeWidth);
+		}
 
     	document.getElementById(id).appendChild(ellipse);
 	}
 
-
-	static rectangle(id, w, h, x, y, fillColor, strokeColor, strokeWidth)
+	static rectangle(id, w, h, x, y, classname, fillColor, strokeColor, strokeWidth)
 	{
     	const rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
 
@@ -66,26 +85,107 @@
     	rect.setAttributeNS(null, 'y', y);
     	rect.setAttributeNS(null, 'height', h);
     	rect.setAttributeNS(null, 'width', w);
-    	rect.setAttributeNS(null, 'fill', fillColor);
-    	rect.setAttributeNS(null, 'stroke', strokeColor);
-    	rect.setAttributeNS(null, 'stroke-width', strokeWidth);
+
+		if(classname !== null)
+		{
+			rect.setAttributeNS(null, "class", classname);
+		}
+		else
+		{
+    		rect.setAttributeNS(null, 'fill', fillColor);
+    		rect.setAttributeNS(null, 'stroke', strokeColor);
+    		rect.setAttributeNS(null, 'stroke-width', strokeWidth);
+		}
 
     	document.getElementById(id).appendChild(rect);
 	}
 
-
-	static text(id, x, y, fontFamily, fontSize, fillColor, strokeColor, text)
+	static text(id, x, y, text, classname, fontFamily, fontSize, fontStyle, fontWeight, fillColor, strokeColor, strokeWidth)
 	{
-    	const XLabel = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    	const txt = document.createElementNS("http://www.w3.org/2000/svg", "text");
 
-    	XLabel.setAttributeNS(null, "x", x);
-    	XLabel.setAttributeNS(null, "y", y);
-    	XLabel.setAttributeNS(null, "fill", fillColor);
-    	XLabel.setAttributeNS(null, "font-family", fontFamily);
-    	XLabel.setAttributeNS(null, "font-size", fontSize);
+    	txt.setAttributeNS(null, "x", x);
+    	txt.setAttributeNS(null, "y", y);
+
+		if(classname !== null)
+		{
+			txt.setAttributeNS(null, "class", classname);
+		}
+		else
+		{
+    		txt.setAttributeNS(null, 'stroke', strokeColor);
+    		txt.setAttributeNS(null, "fill", fillColor);
+    		txt.setAttributeNS(null, "font-family", fontFamily);
+    		txt.setAttributeNS(null, "font-size", fontSize);
+    		txt.setAttributeNS(null, "font-style", fontStyle);
+    		txt.setAttributeNS(null, "font-weight", fontWeight);
+    		txt.setAttributeNS(null, 'stroke-width', strokeWidth);
+		}
 
     	const textNode = document.createTextNode(text);
-    	XLabel.appendChild(textNode);
-    	document.getElementById(id).appendChild(XLabel);
+    	txt.appendChild(textNode);
+    	document.getElementById(id).appendChild(txt);
+	}
+
+	static getStyleElement(id)
+	{
+		const svgelement = document.getElementById(id);
+		let styleelement = svgelement.getElementsByTagName("style").item(0);
+
+		if(styleelement === null)
+		{
+			svgelement.appendChild(document.createElement('style'));
+			styleelement = svgelement.getElementsByTagName("style").item(0);
+		}
+
+		return styleelement;
+	}
+
+	static addLineClass(id, name, strokeColor, strokeWidth)
+	{
+		let classstring = `line.${name}{stroke: ${strokeColor}; stroke-width: ${strokeWidth}px;}`;
+
+		const styleelement = this.getStyleElement(id);
+
+    	styleelement.appendChild(document.createTextNode(classstring));
+	}
+
+	static addCircleClass(id, name, strokeColor, strokeWidth, fillColor)
+	{
+		let classstring = `circle.${name}{stroke: ${strokeColor}; stroke-width: ${strokeWidth}px; fill: ${fillColor};}`;
+
+		const styleelement = this.getStyleElement(id);
+
+    	styleelement.appendChild(document.createTextNode(classstring));
+	}
+
+	static addEllipseClass(id, name, strokeColor, strokeWidth, fillColor)
+	{
+		let classstring = `ellipse.${name}{stroke: ${strokeColor}; stroke-width: ${strokeWidth}px; fill: ${fillColor};}`;
+
+		const styleelement = this.getStyleElement(id);
+
+    	styleelement.appendChild(document.createTextNode(classstring));
+	}
+
+	static addRectangleClass(id, name, strokeColor, strokeWidth, fillColor)
+	{
+		let classstring = `rect.${name}{stroke: ${strokeColor}; stroke-width: ${strokeWidth}px; fill: ${fillColor};}`;
+
+		const styleelement = this.getStyleElement(id);
+
+    	styleelement.appendChild(document.createTextNode(classstring));
+	}
+
+	static addTextClass(id, name, strokeColor, strokeWidth, fillColor, fontFamily, fontSize, fontStyle, fontWeight)
+	{
+		let classstring = `text.${name}
+						   {stroke: ${strokeColor}; stroke-width: ${strokeWidth}px; fill: ${fillColor};
+					   		font-family: ${fontFamily}; font-size: ${fontSize}px;
+							font-style: ${fontStyle}; font-weight: ${fontWeight}}`;
+
+		const styleelement = this.getStyleElement(id);
+
+    	styleelement.appendChild(document.createTextNode(classstring));
 	}
 }
--- svglibrary_demo.js
+++ svglibrary_demo.js
@@ -1,5 +1,37 @@
 window.onload = function()
 {
+    classes();
+
+    // randomPattern();
+}
+
+
+function classes()
+{
+    const w = document.getElementById("svgdiv").offsetWidth;
+    const h = document.getElementById("svgdiv").offsetHeight;
+
+    SVGLibrary.fill("svg1", "#8080FF");
+
+    SVGLibrary.addLineClass("svg1", "redline", "#FF0000", 8);
+    SVGLibrary.line("svg1", 16, h - 16, w - 16, 16, "redline");
+
+    SVGLibrary.addEllipseClass("svg1", "blueellipse", "#0000A0", 6, "#0000FF");
+    SVGLibrary.ellipse("svg1", w / 2, h / 2, (w / 3), h / 3, "blueellipse");
+
+    SVGLibrary.addCircleClass("svg1", "greencircle", "#008000", 4, "#00FF00");
+    SVGLibrary.circle("svg1", w / 2, h / 2, Math.min(w, h)  / 4, "greencircle");
+
+    SVGLibrary.addRectangleClass("svg1", "orangerect", "#FF8000", 2, "#FFC000");
+    SVGLibrary.rectangle("svg1", w / 8, h / 8, (w / 2) - ((w / 8) / 2), (h / 2) - ((h / 8) / 2), "orangerect");
+
+    SVGLibrary.addTextClass("svg1", "purpletext", "#800080", 2, "#C000C0", "serif", 64, "italic", "normal");
+	SVGLibrary.text("svg1", 16, 72, "SVG", "purpletext");
+}
+
+
+function randomPattern()
+{
     const w = document.getElementById("svgdiv").offsetWidth;
     const h = document.getElementById("svgdiv").offsetHeight;
 
@@ -41,7 +73,7 @@ function randomRectangle(w, h)
     const x  = randomBetween(0, w);
     const y  = randomBetween(0, h);
 
-    SVGLibrary.rectangle("svg1", width, height, x, y, fillColor, strokeColor, 1);
+    SVGLibrary.rectangle("svg1", width, height, x, y, null, fillColor, strokeColor, 1);
 }
 
 
@@ -54,7 +86,7 @@ function randomEllipse(w, h)
     const cx  = randomBetween(0, w);
     const cy  = randomBetween(0, h);
 
-    SVGLibrary.ellipse("svg1", cx, cy, rx, ry, fillColor, strokeColor, 1);
+    SVGLibrary.ellipse("svg1", cx, cy, rx, ry, null, fillColor, strokeColor, 1);
 }
 
 
@@ -66,5 +98,5 @@ function randomCircle(w, h)
     const cx  = randomBetween(0, w);
     const cy  = randomBetween(0, h);
 
-    SVGLibrary.circle("svg1", strokeColor, 1,  fillColor, r, cx, cy);
+    SVGLibrary.circle("svg1", cx, cy, r, null, fillColor, strokeColor, 1);
 }
